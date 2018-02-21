0286a6e1a78a417524f810d2baf9ce8a8287d01f
ujjwalmanchanda <ujjwalmanchanda5@gail.com>
2018-02-21 08:54:45 +0530

    latest code for recipe app

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe("Kebab Roll","Description of the recipe", "https://upload.wikimedia.org/wikipedia/commons/e/e7/Kebab_de_la_franquicia_D%27Kebabs%2C_originaria_de_Totana.jpg"),
+    new Recipe("Chicken Biryani","Description of the recipe", "https://upload.wikimedia.org/wikipedia/commons/1/18/Chicken_Biryani_in_Chennai.jpg")
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
